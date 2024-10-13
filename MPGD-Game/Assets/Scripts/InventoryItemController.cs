e3172f7fa33f9428e3560e8812797fabb4266c27
minamiren <minamirensh@gmail.com>
2024-10-13 11:16:18 -0500

    remove and use item capabilities enabled

---
+++ MPGD-Game/Assets/Scripts/InventoryItemController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemController : MonoBehaviour
+{
+    Item item;
+
+    public Button removeButton;
+
+    public void RemoveItem()
+    {
+        InventoryManager.Instance.Remove(item);
+
+        Destroy(gameObject);
+    }
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+    }
+
+    public void UseItem()
+    {
+        Debug.Log(item.value);
+        // TODO: do something with this item value
+        // probably call Player.AddHealth(item.value) or Player.ReduceHunger(item.value) depending on item value
+        InventoryManager.Instance.Remove(item);
+
+        Destroy(gameObject);
+    }
+}
