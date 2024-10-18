e9c6b1c53ac4cf493ca061e1fa88c3afe78596d0
jinyLi-github <jinyuanyouli@gmail.com>
2024-10-19 00:02:19 +0100

    player

---
+++ MPGD-Game/Assets/Player/PlayerScripts/PlayerStates.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Xml.Serialization;
+
+public class PlayerStates : MonoBehaviour
+{
+    public int maxHealth;
+    //public int damage;
+
+    public int maxFullBelly;
+    public float HealthIncreseRate;
+    public float HungerIncreseRate;
+    public float currentHealth;
+    public float currentHunger;
+
+
+
+
+    public Image HealthBar;
+    public Image HungerBar;
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI HungerText;
+    public TextMeshProUGUI gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthIncreseRate = 5f; //Increase Rate
+        HungerIncreseRate = -1f; //decrese Rate
+
+        maxHealth = 100;
+        maxFullBelly = 100;
+        currentHunger = maxFullBelly;
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        //HealthText.text = "HEALTH: " + (int)currentHealth;
+        if (currentHealth <= 0)
+        {
+            PlayerDie();
+        }
+    }
+    public void PlayerDie()
+    {
+        gameState.text = "Game Over!";
+    }
+
+    public void UpdateHealthBar()
+    {
+        HealthText.text = "HEALTH: " + (int)currentHealth;
+        HealthBar.fillAmount = (float)currentHealth / maxHealth;
+
+
+    }
+
+
+//============Hunger with Food==============
+    public void FillBelly(int fill)
+    {
+        currentHunger += fill;
+        currentHunger = Mathf.Clamp(currentHunger, 0, maxFullBelly);
+        HungerText.text = "HUNGER: " + (int)currentHunger;
+    }
+
+    public void hungerByTime()
+    {
+        currentHunger += HungerIncreseRate * Time.deltaTime;
+        currentHunger = Mathf.Clamp(currentHunger, 0, maxFullBelly);
+
+    }
+
+    public void UpdateHunegrBar()
+    {
+        HungerBar.fillAmount = currentHunger / maxFullBelly;
+        HungerText.text = "HUNGER: " + (int)currentHunger;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthBar();
+        hungerByTime();
+        UpdateHunegrBar();
+
+        if (currentHunger <= 0f)
+        {
+            currentHunger = 0f;
+            gameState.text = "Game Over!";
+        }
+
+        if (currentHealth <= 0f)
+        {
+            currentHealth = 0f;
+            gameState.text = "Game Over!";
+        }
+    }
+}
